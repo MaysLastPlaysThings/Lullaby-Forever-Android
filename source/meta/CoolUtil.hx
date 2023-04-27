ccc75c8c6ff8efc3bf775ffbffcd1afdcdeac210
Random people <98900217+justaguy6@users.noreply.github.com>
2023-04-27 07:35:17 -0300

    Update CoolUtil.hx

--- source/meta/CoolUtil.hx
+++ source/meta/CoolUtil.hx
@@ -70,7 +70,7 @@ class CoolUtil
 	{
 		//
 		var libraryArray:Array<String> = [];
-		var unfilteredLibrary = FileSystem.readDirectory('$subDir/$library');
+		var unfilteredLibrary = HSys.readDirectory('$subDir/$library');
 
 		for (folder in unfilteredLibrary)
 		{
