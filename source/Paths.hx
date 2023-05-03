5e3771b0f5db6a412236ffcdaa9f858cccd1dd41
Random people <98900217+justaguy6@users.noreply.github.com>
2023-05-03 08:51:50 -0300

    😐

--- source/Paths.hx
+++ source/Paths.hx
@@ -212,7 +212,7 @@ class Paths
 
 	static public function shader(name:String)
 	{
-		Assets.getText('assets/shaders/$name.frag')
+		return Assets.getText('assets/shaders/$name.frag')
 	}
 
 	static function getPreloadPath(file:String)
