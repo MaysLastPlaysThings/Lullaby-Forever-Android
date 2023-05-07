70bbfd45447257a2023e7c6d68445fe0cd408675
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-05-07 08:50:38 +0800

    cam fix test r1

--- source/meta/state/PlayState.hx
+++ source/meta/state/PlayState.hx
@@ -1558,13 +1558,13 @@ class PlayState extends MusicBeatState
 		return newValue;
 	}
 
-  	 function set_cameraValueMissingno(newValue:Float)
+	function set_cameraValueMissingno(newValue:Float)
 	{
 		cameraValueMissingno = newValue;
 		if (missingnoZoomIn)
 			FlxG.camera.zoom = cameraValueMissingno;
 		return newValue;
-	} //test №1
+	}
 
 	public function flipCharacters()
 	{
@@ -2280,14 +2280,13 @@ class PlayState extends MusicBeatState
 			if (!brimstoneShaking)
 				camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
 
-			var easeLerp = 1 - (elapsed * 3.125);
+			var easeLerp = 1; //1 - (elapsed * 3.125);
 			// camera stuffs
-/*   			if (camZooming)
-			{
+			if (camZooming) {
 				FlxG.camera.zoom = FlxMath.lerp(defaultCamZoom + forceZoom[0] + characterZoom, FlxG.camera.zoom, easeLerp);
 				for (hud in allUIs)
 					hud.zoom = FlxMath.lerp(1 + forceZoom[1], hud.zoom, easeLerp);
-			}*/ //test №3
+			} // Under Testing Cameras - Ralsei
 
 			// not even forcezoom anymore but still
 			FlxG.camera.angle = FlxMath.lerp(0 + forceZoom[2], FlxG.camera.angle, easeLerp);
