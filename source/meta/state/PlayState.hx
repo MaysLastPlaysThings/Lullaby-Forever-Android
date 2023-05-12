995c796d30b27cd381c6d9f7cdd484227af16c47
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-05-12 23:37:00 +0800

    stupid

--- source/meta/state/PlayState.hx
+++ source/meta/state/PlayState.hx
@@ -80,7 +80,7 @@ enum abstract GameModes(String) to String
 class PlayState extends MusicBeatState
 {
 	//additions
-	public var defaultForever:Dynamic = 60.0;
+	public var defaultForeverZoom:Dynamic = 60.0;
 	
 	public var startTimer:FlxTimer;
 
